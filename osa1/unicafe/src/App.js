bcdbeadefa3abe70d51ae8fedcc47bb6a720f713
Jani <jruotinen@gmail.com>
2022-04-22 15:07:01 +0300

    Tehtävä 1.6 done

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -1,5 +1,25 @@
 import { useState } from 'react'
 
+const Header = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Stat = (props) => {
+  return (
+    <p>
+      {props.text} {props.value}
+    </p>
+  )
+}
+
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -8,7 +28,16 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <Header text="Give feedback" />
+      <p>
+        <Button text="Good" handleClick={() => setGood(good+1)} /> 
+        <Button text="Neutral" handleClick={() => setNeutral(neutral+1)} /> 
+        <Button text="Bad" handleClick={() => setBad(bad+1)}/>
+      </p>
+      <Header text="Statistics" />
+      <Stat text="Good" value={good} /> 
+      <Stat text="Neutral" value={neutral} /> 
+      <Stat text="Bad" value={bad} />
     </div>
   )
 }
