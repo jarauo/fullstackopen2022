758fdb9dd1c0c6fae98e27008a1ef3c26cbd5d3d
Jani <jruotinen@gmail.com>
2022-04-28 10:00:37 +0300

    tehtävä 1.14

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const Button = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
 }
 
-export default App;
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
+  ]
+  
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
+
+  const vote = (number) => {
+    let copyVotes = [...votes]
+    copyVotes[number] += 1
+    setVotes(copyVotes)
+  }
+
+  const rndNumber = () => {
+    let number = Math.floor(Math.random() * anecdotes.length)
+    //console.log(number)
+    return number;
+  }
+
+  const indexOfMaxValue = () => {
+    let copyVotes = [...votes]
+    let maxIndex = 0
+
+    for (var i = 0; i < copyVotes.length; i++) {
+      if (copyVotes[i] > copyVotes[maxIndex]) {
+        maxIndex = i
+      }
+    }
+    
+    return maxIndex
+  }
+
+  return (
+    <div>
+      {/*console.log("Votes ",votes)*/}
+      {anecdotes[selected]}
+      <div>
+        <Button text="vote" handleClick={() => {vote(selected)}} />
+        <Button text="next anecdote" handleClick={() => {setSelected(rndNumber)}} />
+      </div>
+      {anecdotes[indexOfMaxValue()]}
+    </div>
+  )
+}
+
+export default App
