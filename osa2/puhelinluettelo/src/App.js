c5d183dfc7ac992c47e97a3aa8aa7f2a79732f52
Jani <jruotinen@gmail.com>
2022-04-29 14:59:15 +0300

    tehtävä 2.11

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,23 +1,28 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
 
+  useEffect(() => {
+    //console.log("Effect")
+    axios
+        .get('http://localhost:3001/persons')
+        .then(response => {
+          //console.log('promise fulfilled')
+          setPersons(response.data)
+        })
+  },[])
+
   const addPerson = (event) => {
     event.preventDefault()
-    console.log('Button clicked', event.target)
+    //console.log('Button clicked', event.target)
 
     const personObject = {
       name: newName,
@@ -35,36 +40,35 @@ const App = () => {
   }
 
   const inputNameChangeHandler = (event) => {
-    console.log("inputNameChangeHandler", event.target.value)
+    //console.log("inputNameChangeHandler", event.target.value)
     setNewName(event.target.value)
   }
 
   const inputNumberChangeHandler = (event) => {
-    console.log("inputNumberChangeHandler", event.target.value)
+    //console.log("inputNumberChangeHandler", event.target.value)
     setNewNumber(event.target.value)
   }
 
   const isPersonAlreadyAdded = (personObject) => {
-    console.log("PersonObject", personObject)
+    //console.log("PersonObject", personObject)
     const index = persons.findIndex(person => person.name === personObject.name)
-    console.log("isPersonAlreadyAdded: ", index === -1 ? false : true)
+    //console.log("isPersonAlreadyAdded: ", index === -1 ? false : true)
     return index === -1 ? false : true
   }
 
   const filterNameHandler = (event) => {
-    console.log("filterNameHandler ",event.target.value)
+    //console.log("filterNameHandler ",event.target.value)
     setFilterName(event.target.value)
   }
 
   return (
     <div>
-      <div>debug: {newName} {newNumber}</div>
       <h2>Phonebook</h2>
       <Filter name={filterName} filterNameHandler={filterNameHandler} />
       <h3>Add a new</h3>
       <PersonForm addPerson={addPerson} 
-                  newName={newName} inputNameChangeHandler={inputNameChangeHandler} 
-                  newNumber={newNumber} inputNumberChangeHandler={inputNumberChangeHandler} 
+        newName={newName} inputNameChangeHandler={inputNameChangeHandler} 
+        newNumber={newNumber} inputNumberChangeHandler={inputNumberChangeHandler} 
       />
       <h2>Numbers</h2>
       <Persons persons={persons} filterName={filterName} />
--- osa2/puhelinluettelo/src/components/Persons.js
+++ osa2/puhelinluettelo/src/components/Persons.js
@@ -4,16 +4,16 @@ const Persons = ({persons, filterName}) => {
     let filteredPersons = persons
 
     const filter = (person) => {
-        console.log("filter:", person)
+        //console.log("filter:", person)
         let index = person.toLowerCase().indexOf(filterName.toLowerCase())
         return index
     }
 
     if (filterName !== '') {
-        filteredPersons = persons.filter(person => console.log(person) || filter(person.name) !== -1)
+        filteredPersons = persons.filter(person => console.log(person.name, filter(person.name)) || filter(person.name) !== -1)
     }
     
-    console.log("filteredPersons:", filteredPersons)
+    //console.log("filteredPersons:", filteredPersons)
 
     return(
         <div>
